47efb086e9db02826e111dbeb68b5e5e082cb788
Vinicius Freitas Pinheiro <viniciusfreitasp@hotmail.com>
2022-08-15 21:53:38 -0300

    leraning-filters

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,17 +1,31 @@
-import filtros from './filtros.json'
-import styles from  './Filtros.module.scss'
+import React from "react";
+import filtros from "./filtros.json";
+import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 
-type IOpcao = typeof filtros[0]
+type IOpcao = typeof filtros[0];
 
-export default function Filtros(){
-    function selecionarFiltro(opcao:IOpcao){
+interface Props {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
 
-    }
-    return <div className={styles.filtros}>
-        {filtros.map((opcao) =>(
-            <button className={styles.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
-                {opcao.label}
-            </button>
-        ))}
+export default function Filtros({ filtro, setFiltro }: Props) {
+
+  function selecionarFiltro(opcao: IOpcao) {
+    if (filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
+  return (
+    <div className={styles.filtros}>
+      {filtros.map(opcao => (
+        <button className={classNames({
+          [styles.filtros__filtro]: true,
+          [styles['filtros__filtro--ativo']]: filtro === opcao.id
+        })} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+          {opcao.label}
+        </button>
+      ))}
     </div>
+  )
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,40 +1,29 @@
-import styles from './Cardapio.module.scss'
-import {ReactComponent as Logo} from 'assets/logo.svg'
-import Buscador from './Buscador'
-import { useState } from 'react'
-import Filtros from './Filtros';
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
 
-
-
-export default function Cardapio (){
-    const [busca,setBusca] = useState("");
-    const [filtro, setFiltro] = useState<number | null >(null);
-
-
-
-    return(
-        <main>
-            <nav className={styles.menu}>
-                <Logo/>
-                
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa do código e da massa
-                </div>
-
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador 
-                    busca ={busca}
-                    setBusca={setBusca}
-                />
-
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro ={setFiltro''} />
-                </div>
-            </section>
-        </main>
-    )
+export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+        </div>
+      </section>
+    </main>
+  )
 }
